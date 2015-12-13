19bdae2676ce83a924302306ba1890130cd3f06e
ejvargasf <enrique.vargas@upr.edu>
2015-12-13 15:31:35 -0400

    Documentation Class PATH.

--- interpreter/src/lib_TEA/Path.java
+++ interpreter/src/lib_TEA/Path.java
@@ -2,7 +2,14 @@ package lib_TEA;
 
 //Immutable Path Class for TEA.
 /**
- * @author Enrique
+ * <h1>PATH</h1>
+ * The <b>PATH</b> class includes all the methods to manage paths
+ * within the TEA language. 
+ * 
+ * TEA Development Team
+ * @author Enddy Y. Gonzales Figueroa
+ * @author Jonathan A. Román Morell
+ * @author Enrique J. Vargas Figueroa
  *
  */
 public class Path {
@@ -20,16 +27,28 @@ public class Path {
 		
 	}
 	
-	/**
-	 * @author 
-	 * @return Returns true if it is an absolute path.
-	 * This method verifies if it is an absolute path.
-	 */
 	public boolean isAbsolute() {
+		/**
+		 * Returns TRUE if Path is an absolute path, FALSE otherwise.
+		 * 
+		 * @return		TRUE if Path is an absolute path.
+		 */
 		return false;
 	}
 	
 	public boolean exists() {
+		/**
+		 * Returns TRUE if Path is an absolute path that exist on disk, FALSE otherwise.
+		 * <p>
+		 * <b>Note:</b> If Path is an empty path, this method returns TRUE because:
+		 * <ul>
+		 * <li>(Windows) the empty path refers to the list of drives mapped to the computer.
+		 * <li>(Mac OS X) the empty path refers to the mounted volumes.
+		 * <li>(Linux) the empty path refers to the root directory.
+		 * </ul>
+		 * 
+		 * @return		TRUE if Path is an absolute path that exist on disk.
+		 */
 		return false;
 	}
 	
@@ -38,6 +57,11 @@ public class Path {
 	}
 	
 	public String getFileName() {
+		/**
+		 * 
+		 * 
+		 * @return		
+		 */
 		return null;
 	}
 	
@@ -47,6 +71,17 @@ public class Path {
 	
 	// If parameter is an absolute Path, there must be an error
 	public Path concat(Path relativePath) {
+		/**
+		 * Creates a new path by appending a name or a relative path) to an existing
+		 * (base) path.
+		 * 
+		 * @param	relativePath	name or relative path is the new path component 
+		 * 							to be appended to the base path. If relativePath
+		 * 							is empty or an invalid path, this function returns
+		 * 							NULL. If the base path is an empty path then
+		 * 							relativePath must be an absolute path.
+		 * @return					Resulting path.
+		 */
 		return null;
 	}
 	
