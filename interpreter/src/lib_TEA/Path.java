ac042f0025679966171e9c2cd4af66346acf413a
John824 <jrm824@hotmail.com>
2015-12-13 16:06:30 -0400

    Started implementation of Path Class.

--- interpreter/src/lib_TEA/Path.java
+++ interpreter/src/lib_TEA/Path.java
@@ -1,23 +1,42 @@
 package lib_TEA;
 
+import java.util.ArrayList;
+
 //Immutable Path Class for TEA.
 /**
  * @author Enrique
  *
  */
 public class Path {
+	
 	private String path;
+	private ArrayList<String> elements;
+	private boolean isAbsolute;
+	private int lenght;
 	
 	
 	public Path(String path) {
 		//TODO Check if Path is valid!!
+		
 		this.path = path;
+		this.elements = new ArrayList<String>();
+		this.isAbsolute = (this.path.charAt(0) == '/');
+		
+		String[] directories = this.path.split("/");
+		
+		for(int i = 0; i < directories.length; i++)
+		{
+			if(!(directories[i] == ""))
+				this.elements.add(directories[i]);
+		}
+		
+		this.lenght = this.elements.size();
+
 	}
 	
 	@Override
 	public String toString() {
 		return path;
-		
 	}
 	
 	/**
@@ -26,15 +45,25 @@ public class Path {
 	 * This method verifies if it is an absolute path.
 	 */
 	public boolean isAbsolute() {
-		return false;
+		return isAbsolute;
 	}
 	
 	public boolean exists() {
 		return false;
 	}
 	
-	public String get() {
-		return null;
+	public String get(int index) {
+		
+		String result = null;
+		
+		if(Math.abs(index) > 0 && Math.abs(index) < lenght)
+		{
+			if(index < 0)
+				result = this.elements.get(this.length() + index);
+			else
+				result = this.elements.get(index);
+		}
+		return result;
 	}
 	
 	public String getFileName() {
@@ -56,12 +85,38 @@ public class Path {
 	
 	public Path select(int index)
 	{
-		return null;
+		if(index < 0)
+		{
+			this.select(this.length() + index, this.length());
+		}
+		return this.select(0, index);
 	}
 	
+	private int length() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 	public Path select(int start, int end)
 	{
-		return null;
+		String newPath = "";
+		
+		
+		if(this.isAbsolute() && start == 0){
+			newPath = "/";
+		}
+		
+		for(int i = start; i <= end; i++)
+		{
+			newPath += this.elements.get(i) + "/";
+		}
+		
+		return new Path(newPath);
+	}
+	
+	public boolean isFile()
+	{
+		return false;
 	}
 
 	public String getExtension() {
--- interpreter/src/testers/MainTerminalTester.java
+++ interpreter/src/testers/MainTerminalTester.java
@@ -9,8 +9,12 @@ public class MainTerminalTester {
 	public static void main(String[] args) {
 		MainTerminal mainTerm = MainTerminal.get();
 		mainTerm.cd(new Path("../"));
-		ArrayList<String> responce = mainTerm.execute("ls");
-		for(String s:responce){
+		ArrayList<String> responce1 = mainTerm.execute("cd ../\nls");
+		ArrayList<String> responce2 = mainTerm.execute("cd ../\nls");
+		for(String s:responce1){
+			System.out.println(s);
+		}
+		for(String s:responce2){
 			System.out.println(s);
 		}
 	}
